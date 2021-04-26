bf8efe7a65ec3653cf6211d6853005bcd757f5e2
Jim Nelson <jim@jimnelson.us>
2021-04-25 22:51:03 -0700

    ILocatable, IMap, IWorld

--- Skeleton.cs
+++ Skeleton.cs
@@ -1,15 +1,35 @@
 using System;
+using System.Collections.Generic;
 
 namespace ProtoFramework
 {
-    public interface ITile
+    public interface ILocatable
     {
         public Coordinates Location{get;}
     }
+    public interface ITile : ILocatable
+    {
+    }
+    public interface IEntity : ILocatable
+    {
+    }
+    public interface IMap
+    {
+        public int Width{get;}
+        public int Height{get;}
+        public int Depth{get;}
+        public IEnumerable<ITile> Tiles{get;}
+        public ITile Tile(ILocatable location);
+    }
+    public interface IWorld
+    {
+        public IMap Map{get;}
+    }
     public class Coordinates : IEquatable<Coordinates>
     {
-        public int X{ get; protected set;}
-        public int Y{ get;  protected set;}
-        public bool Equals(Coordinates other) => other.X == X && other.Y == Y;
+        public int X;
+        public int Y;
+        public int Z;
+        public bool Equals(Coordinates other) => other.X == X && other.Y == Y && other.Z == Z;
     }
 }
